323978fdd8842aac1d8e7ce854868d38bc0166d7
DelangheNiels <delanghe.niels@telenet.be>
2022-06-04 21:20:54 +0200

    start peper component

---
+++ BurgerTime/PeperComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Component.h"
+#include <map>
+#include "PeterPeperComponent.h"
+
+namespace dae
+{
+	class PeperComponent final : public Component
+	{
+	public: 
+		explicit PeperComponent(GameObject* gameObject, RenderComponent* renderComp, std::map<PlayerStates, AnimatedSpriteComponent*> animations);
+		virtual ~PeperComponent() = default;
+		PeperComponent(const PeperComponent & other) = default;
+		PeperComponent(PeperComponent && other) = default;
+		PeperComponent& operator=(const PeperComponent & other) = default;
+		PeperComponent& operator=(PeperComponent && other) = default;
+
+	private:
+		float m_StartX{}, m_StartY{};
+	};
+}
+
+
